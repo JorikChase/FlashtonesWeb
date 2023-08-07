16d1d6966bba32e3b5b4bfe96fda590f6d7f2dc0
JorikChase <71600520+JorikChase@users.noreply.github.com>
2023-08-08 01:15:23 +0200

    ig embed

--- lib/flashtones_web/components/aktuality.ex
+++ lib/flashtones_web/components/aktuality.ex
@@ -1,12 +1,50 @@
 defmodule Aktuality do
   use Phoenix.Component
 
+  instagram_token = Application.compile_env(:flashtones, Flashtones.Instagram)[:access_token]
+
   def aktuality(assigns) do
     ~H"""
 
-      <script>
+      <style>
+        .instagram-feed {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 20px;
+            justify-content: center;
+            padding: 20px;
+            max-width: 1200px;
+            margin: 0 auto;
+          }
 
-        const accessToken = 'IGQVJVSDdzZAkR5cW40YUdxbC13TTJQWmN4QURQZAkFjNFNnTk45aEZANMkYzMWw4V2laMjljQjZA4c1RGVFE4cXBDT0h4TmVXNnlhckVDdl9hZA2pXQ3hhRlEyNTZACaXdlMXZA4aHBzekw3Nkd5eGdMR08xawZDZD';
+          .instagram-post {
+            position: relative;
+            overflow: hidden;
+            border-radius: 10px;
+            box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.1);
+            transition: transform 0.3s ease, box-shadow 0.3s ease;
+          }
+
+          .instagram-post:hover {
+            transform: translateY(-5px);
+            box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.2);
+          }
+
+          .instagram-post img {
+            width: 100%;
+            display: block;
+          }
+
+      </style>
+
+        <div class="instagram-feed">
+          <!-- Posts will be displayed here -->
+        </div>
+
+        <script>
+
+        const accessToken = "#{instagram_token}";
+        console.log(accessToken);
 
           async function fetchInstagramPosts() {
             try {
@@ -53,49 +91,7 @@ defmodule Aktuality do
 
           init();
 
-      </script>
-
-      <style>
-        .instagram-feed {
-            display: grid;
-            grid-template-columns: repeat(3, 1fr);
-            gap: 20px;
-            justify-content: center;
-            padding: 20px;
-            max-width: 1200px;
-            margin: 0 auto;
-          }
-
-          .instagram-post {
-            position: relative;
-            overflow: hidden;
-            border-radius: 10px;
-            box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.1);
-            transition: transform 0.3s ease, box-shadow 0.3s ease;
-          }
-
-          .instagram-post:hover {
-            transform: translateY(-5px);
-            box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.2);
-          }
-
-          .instagram-post img {
-            width: 100%;
-            display: block;
-          }
-
-          .instagram-post.large {
-            grid-column: span 2;
-            grid-row: 2;
-          }
-      </style>
-
-
-      <div class="instagram-feed">
-        <!-- Posts will be displayed here -->
-      </div>
-
-      <script src="script.js"></script>
+        </script>
     """
   end
 end
