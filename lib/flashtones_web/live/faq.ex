3cd288afaece98d984573ae9785451013c65f3b9
JorikChase <71600520+JorikChase@users.noreply.github.com>
2023-08-08 19:46:52 +0200

    faq

--- lib/flashtones_web/live/faq.ex
+++ lib/flashtones_web/live/faq.ex
@@ -3,14 +3,75 @@ defmodule FlashtonesWeb.FaqLive do
 
   def render(assigns) do
     ~H"""
+    <Plachta.plachta />
+    <Menu.menu />
+    <HeadSmall.headSmall />
+    <style>
+      .question-box {
+        cursor: pointer;
+      }
 
-      <Plachta.plachta />
-      <Menu.menu />
-      <HeadSmall.headSmall />
-      <Socky.socky />
-      <Footer.footer />
+      .answer {
+        max-height: 0;
+        overflow: hidden;
+        transition: max-height 0.3s ease-in-out;
+      }
 
+      .active .answer {
+        max-height: 500px; /* Adjust the maximum height as needed */
+      }
+    </style>
+    <div class="container mx-auto px-4 py-8">
+      <h1 class="text-3xl font-bold text-center mb-8">Frequently Asked Questions</h1>
+
+      <div class="w-full md:w-1/2 mx-auto">
+        <div class="mb-4">
+          <h2 class="text-xl font-semibold">Section 1</h2>
+        </div>
+        <div class="question-box p-4 bg-white rounded shadow mb-2">
+          <h3 class="font-semibold">Question 1</h3>
+          <p class="answer mt-2">Answer to Question 1.</p>
+        </div>
+        <div class="question-box p-4 bg-white rounded shadow mb-2">
+          <h3 class="font-semibold">Question 2</h3>
+          <p class="answer mt-2">Answer to Question 2.</p>
+        </div>
+        <div class="question-box p-4 bg-white rounded shadow mb-2">
+          <h3 class="font-semibold">Question 1</h3>
+          <p class="answer mt-2">Answer to Question 1.</p>
+        </div>
+        <div class="question-box p-4 bg-white rounded shadow mb-2">
+          <h3 class="font-semibold">Question 1</h3>
+          <p class="answer mt-2">Answer to Question 1.</p>
+        </div>
+        <!-- Add more question boxes here -->
+        <div class="mb-4">
+          <h2 class="text-xl font-semibold">Section 2</h2>
+        </div>
+        <div class="question-box p-4 bg-white rounded shadow mb-2">
+          <h3 class="font-semibold">Question 1</h3>
+          <p class="answer mt-2">Answer to Question 1.</p>
+        </div>
+        <!-- Add more question boxes here -->
+      </div>
+    </div>
+
+    <script>
+      document.querySelectorAll('.question-box').forEach(box => {
+        box.addEventListener('click', () => {
+          // Toggle the active class on the clicked box
+          box.classList.toggle('active');
+
+          // Hide the answers of other boxes in the same section
+          const otherBoxes = document.querySelectorAll('.question-box:not(.active)');
+          otherBoxes.forEach(otherBox => {
+            otherBox.classList.remove('active');
+          });
+        });
+      });
+    </script>
+    <Socky.socky />
+    <Footer.footer />
     """
   end
-
 end
--- lib/flashtones_web/live/hodnoceni.ex
+++ lib/flashtones_web/live/hodnoceni.ex
@@ -3,14 +3,70 @@ defmodule FlashtonesWeb.HodnoceniLive do
 
   def render(assigns) do
     ~H"""
-
       <Plachta.plachta />
       <Menu.menu />
       <HeadSmall.headSmall />
+
+      <!-- Ratings Page -->
+      <div class="container mx-auto py-8">
+        <h2 class="text-2xl font-bold mb-4">Ratings</h2>
+
+        <!-- Five Star Scale -->
+        <div class="flex items-center space-x-1 mb-4">
+          <div class="text-gray-500">Rate:</div>
+          <div class="flex items-center">
+            <!-- Star Icons (replace with your star icons) -->
+            <span class="text-yellow-500">*</span>
+            <span class="text-yellow-500">*</span>
+            <span class="text-yellow-500">*</span>
+            <span class="text-yellow-500">*</span>
+            <span class="text-yellow-500">*</span>
+          </div>
+        </div>
+
+        <!-- Total Ratings and Average Score -->
+        <div class="text-gray-500">
+          Total Ratings: <span class="font-bold"><%= @total_ratings %></span>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          Average Score: <span class="font-bold"><%= @average_score %></span>
+        </div>
+      </div>
+
+      <!-- Seamless Grid for Critiques and Opinions -->
+      <div class="container mx-auto py-8">
+        <h2 class="text-2xl font-bold mb-4">User Critiques</h2>
+
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <%= for {rating, critique, name, profession} <- @critiques do %>
+            <div class="bg-gray-100 p-4 rounded shadow-md">
+              <div class="flex items-center space-x-2 mb-2">
+                <div class="text-yellow-500">*</div> <!-- User Rating (replace with actual rating) -->
+                <div class="text-sm text-gray-500">(<%= rating %>)</div> <!-- User Rating in Numeric -->
+              </div>
+              <p class="text-gray-700 mb-2"><%= critique %></p>
+              <div class="text-gray-500"><%= name %>, <%= profession %></div>
+            </div>
+          <% end %>
+        </div>
+      </div>
+
       <Socky.socky />
       <Footer.footer />
-
     """
   end
 
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, total_ratings: 0, average_score: 0, critiques: [])}
+  end
+
+  def handle_event("rate", %{"score" => score}, socket) do
+    critique = "User Opinion Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    critiques = [
+      {String.to_integer(score), critique, "Name", "Profession"} | socket.assigns.critiques
+    ]
+    total_ratings = length(critiques)
+    average_score = Enum.sum(critiques, fn {rating, _, _, _} -> rating end) / total_ratings
+
+    {:noreply, assign(socket, total_ratings: total_ratings, average_score: average_score, critiques: critiques)}
+  end
 end
